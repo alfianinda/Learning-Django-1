6040a33e254b11318636316115ee756072ddf217
alfianinda <ni.alfianinda@gmail.com>
2020-02-04 18:00:32 +0800

    [Alfianinda] - dictionary

---
+++ dictionary.py
@@ -0,0 +1,23 @@
+dogs = {"Fido":8, "Sally":17, "Sean":2}
+print(dogs)
+
+# call 
+print(dogs["Fido"])
+
+# delete
+del(dogs["Fido"])
+print(dogs)
+
+# add
+dogs["Sarah"] = 6
+print(dogs)
+
+#update
+dogs["Sean"] = 100
+print(dogs)
+
+#all types
+dogs["John"] = True
+print(dogs)
+
+#Exercise
