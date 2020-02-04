202bf67a00c9e7e9a8b5320a122787adca237fd4
alfianinda <ni.alfianinda@gmail.com>
2020-02-04 11:41:35 +0800

    [Alfianinda] - variable2

--- pythonrefresh.py
+++ pythonrefresh.py
@@ -1,5 +1,19 @@
-age = 17
-name = "Inna"
+#python pythonrefresh.py
 
-print(age)
-print("Hello my name is {}. I am from Mars".format(name))
+# age = 17
+# name = "Inna"
+
+# print(age)
+# print("Hello my name is {}. I am {} years old from Mars".format(name, age))
+
+# exercise 1 ##########################
+# I have given you two variables, name and age. Use the format() function to create a sentence that reads:
+# Hi my name is Julie and I am 42 years old
+# Set that equal to the variable called sentence
+
+name = "Julie"
+age = 42
+sentence = "Hi my name is {} and I am {} years old".format(name, age)
+print(sentence)
+
+
