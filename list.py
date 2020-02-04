e56a27936166c4dcf311a7f6ae7b744511fb4f93
alfianinda <ni.alfianinda@gmail.com>
2020-02-04 17:43:08 +0800

    [Inna] - loop

---
+++ list.py
@@ -0,0 +1,35 @@
+# insert
+# dognames = ["Fido", "Sean", "Sally", "Mark"]
+# dognames.insert(0, "Jane")
+# print(dognames)
+# print(dognames[2])
+
+# delete
+# dognames = ["Fido", "Sean", "Sally", "Mark"]
+# del(dognames[2])
+# print(dognames)
+
+# len
+# dognames = ["Fido", "Sean", "Sally", "Mark"]
+# print(len(dognames))
+
+# replace
+# dognames = ["Fido", "Sean", "Sally", "Mark"]
+# dognames[1] = "Jane"
+# print(dognames)
+
+# contain of array
+# dognames = ["Fido", "Sean", "Sally", "Mark", 423, False, True, 4.656]
+# print(dognames)
+
+# Exercise
+# Create a variable named shoes that is a list of the follow items, in order:
+# Spizikes
+# Air Force 1
+# Curry 2
+# Melo 5
+
+shoes = ["Spizikes", "Air Force 1", "Curry 2", "Melo 5"]
+print(shoes)
+
+
---
+++ loop.py
@@ -0,0 +1,25 @@
+# # for
+
+# dognames = ["Fido", "Sean", "Sally", "Mark"]
+
+# for dog in dognames:
+#     print(dog)
+
+# for x in range(1,10):
+#     print(x)
+
+# # while
+
+# age = 0
+
+# while age < 18:
+#     print(age)
+#     age += 1
+
+# exercise 
+
+numbers = [23, 26, 91, 95, 98, 100]
+
+for i in numbers:
+    if i > 90:
+        print(i)
