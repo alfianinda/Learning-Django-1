e1798f24ccacd7b64a82b02a70237de40efeeb66
alfianinda <ni.alfianinda@gmail.com>
2020-02-04 12:28:50 +0800

    [Alfianinda] - if statements

--- pythonrefresh.py
+++ pythonrefresh.py
@@ -1,4 +1,6 @@
-#python pythonrefresh.py
+# python pythonrefresh.py
+
+### variable 
 
 # age = 17
 # name = "Inna"
@@ -6,14 +8,38 @@
 # print(age)
 # print("Hello my name is {}. I am {} years old from Mars".format(name, age))
 
-# exercise 1 ##########################
+### exercise 1 
 # I have given you two variables, name and age. Use the format() function to create a sentence that reads:
 # Hi my name is Julie and I am 42 years old
 # Set that equal to the variable called sentence
 
-name = "Julie"
-age = 42
-sentence = "Hi my name is {} and I am {} years old".format(name, age)
-print(sentence)
+# name = "Julie"
+# age = 42
+# sentence = "Hi my name is {} and I am {} years old".format(name, age)
+# print(sentence)
 
+### if statements and comments 
+age = 12 
+name = "Matt"
 
+todayIsCold = True
+
+# if age > 18: 
+#     print("You are older than 18")
+#     print("This is another line")
+# else:
+#     print("You are younger than 18")
+
+# if todayIsCold: 
+#     print("Today is cold")
+# else:
+#     print("Today is hot")
+
+# if name == "Matt": 
+#     print("My name is Matt")
+# else:
+#     print("My name is Inna")
+
+# single comment
+""" multiple rows 
+of comment """
