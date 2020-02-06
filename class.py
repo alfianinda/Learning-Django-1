54051c9382e2eba60faa455eb02718de88162d1e
alfianinda <ni.alfianinda@gmail.com>
2020-02-06 17:54:47 +0800

    [Alfianinda] - django intro

--- class.py
+++ class.py
@@ -4,8 +4,8 @@
 #     def bark(self, str):
 #         print("BARK!!" + str)
 
-# mydog = Dog()
-# # mydog.bark()
+# mydog = Dog() #new Dog
+# mydog.bark()
 # mydog.bark("guk guk guk")
 
 # mydog.name = "Fido"
@@ -18,20 +18,26 @@
 
 ### init method
 
-class Dog:
-    dogInfo = "Hey dogs are cool"
+# class Dog:
+#     def __init__(self, name, age, furcolor):
+#         self.name = name
+#         self.age = age
+#         self.furcolor = furcolor
 
-    def bark(self, str):
-        print("BARK!!" + str)
+#     def bark(self, str):
+#         print("BARK" + str)
 
-mydog = Dog()
-# mydog.bark()
-mydog.bark("guk guk guk")
+# mydog = Dog() #new Dog
+# print(mydog.age)
 
-mydog.name = "Fido"
-mydog.age = 16
-print(mydog.name)
-print(mydog.age)
+### Exercise
+# Add a method to the Car class called age that returns how old the car is (2018 - year)
+# *Be sure to return the age, not print it*
 
-Dog.dogInfo = "Hey there"
-print(Dog.dogInfo)
+class Car:
+    def age(self, year):
+        return "2018 - {}".format(year)
+
+yearCar = Car()
+print(yearCar.age(2019))
+
