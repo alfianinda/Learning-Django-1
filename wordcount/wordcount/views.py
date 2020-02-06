3656579e55f2f603e7ab1d809666c83c86fa1ee8
alfianinda <ni.alfianinda@gmail.com>
2020-02-06 23:30:17 +0800

    [Alfianinda] - django forms

--- wordcount/wordcount/views.py
+++ wordcount/wordcount/views.py
@@ -1,8 +1,15 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-def home(request):
-    return render(request, "home.html", {'hithere':'This is me'})
+# def home(request):
+#     return render(request, "home.html", {'hithere':'This is me'})
 
-def eggs(request):
-    return HttpResponse("<h1>Eggs are great!<h1>")
+# def eggs(request):
+#     return HttpResponse("<h1>Eggs are great!<h1>")
+
+def home(request):
+    return render(request, "home.html")
+
+def count(request):
+    return render(request, "count.html")
+
