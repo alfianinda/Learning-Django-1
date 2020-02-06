c49677bbd86fcc53350e1ad7364c62e9618d0345
alfianinda <ni.alfianinda@gmail.com>
2020-02-06 23:03:21 +0800

    [Alfianinda] - django tempaltes

--- wordcount/wordcount/views.py
+++ wordcount/wordcount/views.py
@@ -1,7 +1,8 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def home(request):
-    return HttpResponse("Hello")
+    return render(request, "home.html", {'hithere':'This is me'})
 
 def eggs(request):
     return HttpResponse("<h1>Eggs are great!<h1>")
