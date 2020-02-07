0b4cd7fa84b3c7adb48b66855e75a0b23edb382b
alfianinda <ni.alfianinda@gmail.com>
2020-02-07 17:49:57 +0800

    [Alfianinda] - counting the words web

--- wordcount/wordcount/views.py
+++ wordcount/wordcount/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+import operator
 
 # def home(request):
 #     return render(request, "home.html", {'hithere':'This is me'})
@@ -11,5 +12,21 @@ def home(request):
     return render(request, "home.html")
 
 def count(request):
-    return render(request, "count.html")
+    fulltext = request.GET["fulltext"]
+    print(fulltext)
+    wordlist = fulltext.split()
+    print(wordlist)
+
+    worddictionary = {}
+    for word in wordlist:
+        if word in worddictionary:
+            worddictionary[word] += 1
+        else:
+            worddictionary[word] = 1
+
+    sortedwords = sorted(worddictionary.items(), key=operator.itemgetter(1), reverse=True)
+
+    # return render(request, "count.html", {"fulltext":fulltext, "count":len(wordlist), "worddictionary":worddictionary})
+    # return render(request, "count.html", {"fulltext":fulltext, "count":len(wordlist), "worddictionary":worddictionary.items()}) #items() making list
+    return render(request, "count.html", {"fulltext":fulltext, "count":len(wordlist), "sortedwords":sortedwords}) 
 
